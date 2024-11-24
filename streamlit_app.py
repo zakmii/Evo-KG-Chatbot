88caa1e6700b825bc5ed02e8f105ba2d56edfcb6
Ankit Singh <ankit21450&iiitd.ac.in>
2024-11-24 22:18:57 +0530

    changed (increased) auto_summarize_buffer_tokens to 10000

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def initialize_session_state():
     if "agents" not in st.session_state:
         st.session_state.agents = {
             "EvoKG Assistant": {
-                "agent": EvoKgAgent("EvoKG Assistant", model="gpt-4o-mini", openai_api_key=get_current_api_key_for_agent_use(), auto_summarize_buffer_tokens=500),
+                "agent": EvoKgAgent("EvoKG Assistant", model="gpt-4o-mini", openai_api_key=get_current_api_key_for_agent_use(), auto_summarize_buffer_tokens=10000),
                 "greeting": greeting,
                 "avatar": "ℹ️",
                 "user_avatar": "👤",
