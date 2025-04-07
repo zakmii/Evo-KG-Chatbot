c0e0f7110022a8d10179ab450253fbcdbac10245
zakmii <ankit21450@iiitd.ac.in>
2025-04-07 15:42:58 +0530

    add HuggingFace engines and update agent dictionary in streamlit_app_2.py

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -2,6 +2,8 @@ import kani_utils.kani_streamlit_server as ks
 import os
 import dotenv
 from kani.engines.openai import OpenAIEngine
+from kani.engines.huggingface import HuggingEngine
+
 from agents import EvoKgAgent
 import dotenv
 
@@ -25,13 +27,16 @@ ks.initialize_app_config(
 
 # define an engine to use (see Kani documentation for more info)
 engine = OpenAIEngine(os.environ["OPENAI_API_KEY"], model="gpt-4o-mini")
-
+tinyAgentEngine = HuggingEngine(model_id="driaforall/Tiny-Agent-a-0.5B", device="cuda:0")
+mistralEngine = HuggingEngine(model_id="mistralai/Mistral-7B-Instruct-v0.3", device="cuda:0")
 
 # We also have to define a function that returns a dictionary of agents to serve
 # Agents are keyed by their name, which is what the user will see in the UI
 def get_agents():
     return {
             "EvoLLM (4o-mini)": EvoKgAgent(engine), #, prompt_tokens_cost = 0.005, completion_tokens_cost = 0.015),
+            "EvoLLM (Tiny-Agent)": EvoKgAgent(tinyAgentEngine),
+            "EvoLLM (Mistral)": EvoKgAgent(mistralEngine),
            }
 
 
