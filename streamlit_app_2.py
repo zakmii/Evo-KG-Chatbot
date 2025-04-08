bf4b351e97c0ede51faec255017d89508d59d363
zakmii <ankit21450@iiitd.ac.in>
2025-04-08 12:26:29 +0530

    update function call of ks

--- streamlit_app_2.py
+++ streamlit_app_2.py
@@ -7,35 +7,40 @@ from kani.engines.huggingface import HuggingEngine
 from agents import EvoKgAgent
 import dotenv
 
-dotenv.load_dotenv() 
+dotenv.load_dotenv()
 
 # initialize the application and set some page settings
-# parameters here are passed to streamlit.set_page_config, 
+# parameters here are passed to streamlit.set_page_config,
 # see more at https://docs.streamlit.io/library/api-reference/utilities/st.set_page_config
 # this function MUST be run first
 ks.initialize_app_config(
-    show_function_calls = False,
-    page_title = "EvoLLM",
-    page_icon = "🧬", # can also be a URL
-    initial_sidebar_state = "expanded",
-    menu_items = {
-            "Get Help": "https://github.com/monarch-initiative/phenomics-assistant",
-            "Report a Bug": "https://github.com/monarch-initiative/phenomics-assistant/issues",
-            "About": "EvoLLM is built on GPT-4o-mini, Streamlit, zhudotexe/kani, hourfu/redlines, and oneilsh/kani-utils.",
-        }
+    show_function_calls=False,
+    page_title="EvoLLM",
+    app_title="EvoLLM",
+    logo_path="logo.png",
+    page_icon="🧬",  # can also be a URL
+    initial_sidebar_state="expanded",
+    menu_items={
+        "Get Help": "https://github.com/monarch-initiative/phenomics-assistant",
+        "Report a Bug": "https://github.com/monarch-initiative/phenomics-assistant/issues",
+        "About": "EvoLLM is built on GPT-4o-mini, Streamlit, zhudotexe/kani, hourfu/redlines, and oneilsh/kani-utils.",
+    },
 )
 
 # define an engine to use (see Kani documentation for more info)
 engine = OpenAIEngine(os.environ["OPENAI_API_KEY"], model="gpt-4o-mini")
-#tinyAgentEngine = HuggingEngine(model_id="driaforall/Tiny-Agent-a-0.5B")
+# tinyAgentEngine = HuggingEngine(model_id="driaforall/Tiny-Agent-a-0.5B")
+
 
 # We also have to define a function that returns a dictionary of agents to serve
 # Agents are keyed by their name, which is what the user will see in the UI
 def get_agents():
     return {
-            "EvoLLM (4o-mini)": EvoKgAgent(engine), #, prompt_tokens_cost = 0.005, completion_tokens_cost = 0.015),
-            #"EvoLLM (Tiny-Agent)": EvoKgAgent(tinyAgentEngine),
-           }
+        "EvoLLM (4o-mini)": EvoKgAgent(
+            engine
+        ),  # , prompt_tokens_cost = 0.005, completion_tokens_cost = 0.015),
+        # "EvoLLM (Tiny-Agent)": EvoKgAgent(tinyAgentEngine),
+    }
 
 
 # tell the app to use that function to create agents when needed
